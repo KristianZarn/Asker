1588bf65cfcf955dcd78a1f549a4d9fe86f2a223
Kristian Zarn <kristian.zarn@gmail.com>
2013-12-30 15:15:55 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AskerRuby::Application.config.secret_key_base = '8d9f44ec8cf208fb7de0371204e149c77e8a2421445a599af8f51316e0a301900a178bdbeb04c2c45a46e44e159715993e3b204566986b8a3ba276f38240e55f'
